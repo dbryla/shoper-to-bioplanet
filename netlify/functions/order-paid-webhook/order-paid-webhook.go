ce26d1255f2b1183169250a1e47ecc0a1e6a55ae
Daniel Bryla <dan.bryla@gmail.com>
2022-03-04 18:20:04 +0100

    Secure endpoint

--- netlify/functions/order-paid-webhook/order-paid-webhook.go
+++ netlify/functions/order-paid-webhook/order-paid-webhook.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/sha1"
-	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -10,8 +9,12 @@ import (
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	hash := sha1.New()
 	hash.Write([]byte(request.Headers["x-webhook-id"] + ":test-123:" + request.Body))
-	fmt.Println("Expected " + request.Headers["x-webhook-sha1"])
-	fmt.Printf("Actual: %x\n", hash.Sum(nil))
+	checksum := string(hash.Sum(nil)[:])
+	if checksum != request.Headers["x-webhook-sha1"] {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 401,
+		}, nil
+	}
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 	}, nil
