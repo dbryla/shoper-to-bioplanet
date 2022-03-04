7480cd93b055ec99d8cd946d738cfdd83035c2cc
Daniel Bryla <dan.bryla@gmail.com>
2022-03-04 18:34:21 +0100

    Extract secret to env variable and small refactor

--- netlify/functions/order-paid-webhook/order-paid-webhook.go
+++ netlify/functions/order-paid-webhook/order-paid-webhook.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"crypto/sha1"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
+	"strings"
 )
 
+var secret = os.Getenv("API_KEY")
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	hash := sha1.New()
-	hash.Write([]byte(request.Headers["x-webhook-id"] + ":test-123:" + request.Body))
-	checksum := string(hash.Sum(nil)[:])
+	checksum := calculateChecksum(request)
 	if checksum != request.Headers["x-webhook-sha1"] {
+		fmt.Println("Rejected.")
 		return &events.APIGatewayProxyResponse{
 			StatusCode: 401,
 		}, nil
 	}
+	fmt.Println("Authenticated.")
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 	}, nil
+}
+
+func calculateChecksum(request events.APIGatewayProxyRequest) string {
+	sb := strings.Builder{}
+	sb.WriteString(request.Headers["x-webhook-id"])
+	sb.WriteString(":")
+	sb.WriteString(secret)
+	sb.WriteString(":")
+	sb.WriteString(request.Body)
+	hash := sha1.New()
+	hash.Write([]byte(sb.String()))
+	checksum := string(hash.Sum(nil)[:])
+	return checksum
 }
 
 func main() {
