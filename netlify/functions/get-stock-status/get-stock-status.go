5f65748ec0dec652b64675996f7c89c09fc57329
Daniel Bryla <dan.bryla@gmail.com>
2022-03-06 11:35:39 +0100

    Add get stock status function with api key authentication

---
+++ netlify/functions/get-stock-status/get-stock-status.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"os"
+)
+
+var apiKey = os.Getenv("API_KEY")
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	if isNotAuthenticated(request) {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 401,
+		}, nil
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
+}
+
+func isNotAuthenticated(request events.APIGatewayProxyRequest) bool {
+	return request.QueryStringParameters["api-key"] == apiKey
+}
